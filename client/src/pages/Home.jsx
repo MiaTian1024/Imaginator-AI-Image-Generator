a25310319783aad35191b0e15d5dbba39137157b
MiaTian1024 <sddxtm123@gmail.com>
2023-05-16 18:30:41 +1200

    Working on localhost

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,8 +18,48 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null)
 
   const [searchText, setSearchText] = useState('');
- 
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
   
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if(response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse())
+      }
+    } catch (err) {
+      alert(err)
+    } finally {
+      setLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
+  };
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -53,12 +93,12 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ? (
                 <RenderCards 
-                  data={[]}
+                  data={searchedResults}
                   title="No search results found"
                 />
               ) : (
                 <RenderCards 
-                  data={[]}
+                  data={allPosts}
                   title="No posts found"
                 />
               )}
