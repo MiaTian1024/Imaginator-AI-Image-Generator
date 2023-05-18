d1598a1179d2d77e53c0ad951ce9dd6c7642de05
MiaTian1024 <sddxtm123@gmail.com>
2023-05-18 15:54:20 +1200

    update logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,18 @@ import { Home, CreatePost } from './pages'
 
 const App = () => (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center bg-primary sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain" />
+          <div className='w-full flex justify-between items-center'>
+          <img src={logo} alt="logo" className="w-12 object-contain" />
+          <p className="font-extrabold text-secondary text-[22px] px-2 pt-2">Imaginator</p>
+          </div>
         </Link>
-
-        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+        
+        <div className='flex justify-between'>
+        <Link to="/create-post" className="font-bold text-secondary mx-2 px-4 py-2">Community</Link>
+        <Link to="/create-post" className="font-bold text-secondary mx-2 px-4 py-2">Create</Link>
+        </div>
       </header>
       
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
