1eaed1bbe65839a3fc7512db51bfa6197fef0125
MiaTian1024 <sddxtm123@gmail.com>
2023-05-22 13:09:55 +1200

    Tilt effect for cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import {Tilt} from 'react-tilt'
+import { motion } from 'framer-motion'
 
 import { download } from '../assets';
 import { downloadImage } from '../utils';
+import { fadeIn } from '../utils/motion'
 
 
-const Card = ({ _id, name, prompt, photo}) => (
-    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+const Card = ({ _id, name, prompt, photo}) => {
+  return (
+      <Tilt
+        options={{
+          max:45,
+          scale:1,
+          speed:450
+        }}
+        className="rounded-xl group relative shadow-card hover:shadow-cardhover card"
+      >
+    <div>
       <img
         className="w-full h-auto object-cover rounded-xl"
         src={photo}
         alt={prompt}
       />
-      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-primary m-2 p-4 rounded-md">
         <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
 
         <div className="mt-5 flex justify-between items-center gap-2">
@@ -19,12 +31,12 @@ const Card = ({ _id, name, prompt, photo}) => (
             <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">{name[0]}</div>
             <p className="text-white text-sm">{name}</p>
           </div>
-          <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none">
-            <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
+          <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-white border-none">
+            <img src={download} alt="download" className="w-7 h-7 object-contain" />
           </button>
         </div>
       </div>
     </div>
-  );
-
+    </Tilt>
+)}
 export default Card;
