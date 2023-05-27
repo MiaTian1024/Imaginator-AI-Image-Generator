469a01812b14cae83e7357127ab07dfa90358b8e
MiaTian1024 <sddxtm123@gmail.com>
2023-05-27 22:10:17 +1200

    Update FormField.jsx

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -25,7 +25,7 @@ const FormField = ({
             onClick={handleSurpriseMe}
             className="font-semibold text-xs bg-green-700 py-1 px-2 rounded-[5px] text-white"
           >
-            Suprise me
+            Surprise me
           </button>
         )}
       </div>
